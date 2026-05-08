505ec4208b82abc7944cb5723e38d25c647c782c
Aman Lama <lamaaman2036@gmail.com>
2026-05-08 20:21:31 +0545

    fix Service casing

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import About from "../components/About";
 import Blogs from "../components/Blogs";
 import GetInTouch from "../components/GetInTouch";
 import Hero from "../components/Hero";
-import Service from "../components/service";
+import Service from "../components/Service";
 import SpecialityMenu from "../components/SpecialityMenu";
 import TopDoctors from "../components/TopDoctors";
 
